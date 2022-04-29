6ff8390e2ba0e8d9e6e3a4e43e6a5379f9ae1ac1
Michal Rzeznik <90emer@gmail.com>
2022-04-29 13:45:49 +0200

    interface specfification updated; fixed project references in tests;

--- Library.Tools/IFileAccess.cs
+++ Library.Tools/IFileAccess.cs
@@ -5,7 +5,7 @@ namespace Library.Tools
     /// <summary>Interface defining methods for flat file access.</summary>
     public interface IFileAccess<T>
     {
-        /// <summary>Reads data from file of given T type.</summary>
+        /// <summary>Reads data from file of given T type. If file is not found, or is empty, empty collection will be returned.</summary>
         IEnumerable<T> Read(string filePath);
 
         /// <summary>Append single entity of T type data.</summary>
